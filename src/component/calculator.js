a75daf3dec81dc4ee5528622e868051558404b2d
joel <joelkalema63@gmail.com>
2022-04-21 00:05:54 +0200

    Update the README file

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -23,38 +23,28 @@ class Calculator extends React.Component {
       result = `${next} ${op || ''}`;
     }
     return (
-      <div className="UI">
-        <div className="screen">{result || 0}</div>
-        <div className="body">
-          <div className="body-key">
-            <div className="reset">
-              <button type="button" onClick={updateDetails}>AC</button>
-              <button type="button" value="+/-" onClick={updateDetails}>+/-</button>
-              <button type="button" value="%" onClick={updateDetails}>%</button>
-            </div>
-            <div className="buttons">
-              <button type="button" onClick={updateDetails}>7</button>
-              <button type="button" onClick={updateDetails}>8</button>
-              <button type="button" onClick={updateDetails}>9</button>
-              <button type="button" onClick={updateDetails}>4</button>
-              <button type="button" onClick={updateDetails}>5</button>
-              <button type="button" onClick={updateDetails}>6</button>
-              <button type="button" onClick={updateDetails}>1</button>
-              <button type="button" onClick={updateDetails}>2</button>
-              <button type="button" onClick={updateDetails}>3</button>
-              <div className="footer">
-                <button type="button" className="zero" onClick={updateDetails}>0</button>
-                <button type="button" className="point" onClick={updateDetails}>.</button>
-              </div>
-            </div>
-          </div>
-          <div className="sign">
-            <button type="button" onClick={updateDetails}>÷</button>
-            <button type="button" onClick={updateDetails}>x</button>
-            <button type="button" onClick={updateDetails}>-</button>
-            <button type="button" onClick={updateDetails}>+</button>
-            <button type="button" onClick={updateDetails}>=</button>
-          </div>
+      <div className="container">
+        <div className="calculator">
+          <div className="screen">{result || 0}</div>
+          <button type="button" onClick={updateDetails}>AC</button>
+          <button type="button" onClick={updateDetails}>+/-</button>
+          <button type="button" onClick={updateDetails}>%</button>
+          <button type="button" onClick={updateDetails}>÷</button>
+          <button type="button" onClick={updateDetails}>7</button>
+          <button type="button" onClick={updateDetails}>8</button>
+          <button type="button" onClick={updateDetails}>9</button>
+          <button type="button" onClick={updateDetails}>x</button>
+          <button type="button" onClick={updateDetails}>4</button>
+          <button type="button" onClick={updateDetails}>5</button>
+          <button type="button" onClick={updateDetails}>6</button>
+          <button type="button" onClick={updateDetails}>-</button>
+          <button type="button" onClick={updateDetails}>1</button>
+          <button type="button" onClick={updateDetails}>2</button>
+          <button type="button" onClick={updateDetails}>3</button>
+          <button type="button" onClick={updateDetails}>-</button>
+          <button type="button" onClick={updateDetails}>.</button>
+          <button type="button" onClick={updateDetails}>0</button>
+          <button type="button" className="equal" onClick={updateDetails}>=</button>
         </div>
       </div>
     );

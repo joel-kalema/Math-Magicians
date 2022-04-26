b26c7d6102b548f3811d837941dba366db842547
joel <joelkalema63@gmail.com>
2022-04-26 12:50:38 +0200

    Added the nav component

---
+++ src/component/navBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Calculator</li>
+      <li>Quotes</li>
+    </ul>
+  </nav>
+);
+
+export default Nav;

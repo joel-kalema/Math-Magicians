9eef954b4d21b617fd17d5008d0ea7df254ed71d
joel <joelkalema63@gmail.com>
2022-04-26 23:56:14 +0200

    Added the style

--- src/component/Quote.js
+++ src/component/Quote.js
@@ -6,8 +6,8 @@ const Quote = () => (
   <div className="quote">
     <div className="quote-container">
       <h1>Why are mathematicians so important?</h1>
-      <p>Mathematicians are essential to analyze data and design accurate models in fields as</p>
-      <p>diverse as biology an finance.</p>
+      <p>Mathematicians are essential to analyze data and design accurate models</p>
+      <p>in fields as diverse as biology an finance.</p>
       <p>Mathematicians enabled researchers to complete The Human Genome Project quickly.</p>
     </div>
   </div>
